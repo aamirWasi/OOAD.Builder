0f6cc8ae08298c810862b60d5e03bf4a870e4892
aamir <wasi3aamir@gmail.com>
2020-05-01 20:02:43 +0600

    Add project files.

---
+++ OOD.Builder.OOAD.B1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOD.Builder.OOAD.B1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
