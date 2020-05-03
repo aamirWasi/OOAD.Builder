e0bbc7086703879c6fd14ce4acac711e53b934cc
aamir <wasi3aamir@gmail.com>
2020-05-03 19:27:20 +0600

    now main program works with library project

---
+++ OOD.RPG.FluentInterface/CommonHelper.cs
@@ -0,0 +1,85 @@
+﻿using OOD.RPG.FluentInterface.Types;
+using System;
+using System.Threading;
+
+namespace OOD.RPG.FluentInterface
+{
+    public static class CommonHelper
+    {
+        private static string[] _firstNames =
+        {
+            "Destro","Victo","Mozri","Fang","Ovi","Hell","Syth","End"
+        };
+        private static string[] _lastNames =
+        {
+            "math","rin","sith","icous","ravage","wrath","ryn","less"
+        };
+        private static string[] _attackVerbs =
+        {
+              "Slashes","Stabs","Smashes","Impales","Poisons","Shoots","Incinerates","Destroys"
+        };
+
+        public static  string GenerateRandomName()
+        {
+            string result = "";
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            result = _firstNames[ran.Next(_firstNames.Length)] + _lastNames[ran.Next(_lastNames.Length)];
+            return result;
+        }
+
+        public static void DisplayStartOfBattle(Character hero,Character enemy)
+        {
+            Console.WriteLine("=Starting Battle=");
+            Console.WriteLine(hero.ToString());
+            Console.WriteLine();
+            Console.WriteLine("VS."); 
+            Console.WriteLine();
+            Console.WriteLine(enemy.ToString());
+            Console.WriteLine();
+            Console.WriteLine("An enemy approaches >");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static void Battle(Character hero,Character enemy)
+        {
+            while (hero.IsAlive && enemy.IsAlive)
+            {
+                Console.WriteLine($"{hero.QuickStats()} / {enemy.QuickStats()}");
+                Thread.Sleep(1000);
+                Console.WriteLine(Attack(hero,enemy));
+                Thread.Sleep(1000);
+                Console.WriteLine(Attack(enemy,hero));
+                if (hero.IsAlive)
+                {
+                    Console.Write(">");
+                    Console.ReadKey();
+                }
+                Console.WriteLine();
+            }
+            if (hero.IsAlive)
+            {
+                hero.Gold += enemy.Gold;
+                Console.WriteLine($"Our hero survives to fight another battle!Won {enemy.Gold} gold");
+                Console.WriteLine(">");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine($"Our hero has fallen with {hero.Gold} gold! .The world is covered in darkness once again");
+            }
+        }
+
+        private static string Attack(Character attacker, Character defender)
+        {
+            string result = "";
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            int damage = ran.Next(10);
+            defender.HP -= damage;
+            string verbs = _attackVerbs[ran.Next(_attackVerbs.Length)];
+            result = $"{attacker.Name} {verbs} {defender.Name} for {damage} damage";
+            return result;
+        }
+    }
+}
---
+++ OOD.RPG.FluentInterface/Program.cs
@@ -0,0 +1,39 @@
+﻿using OOD.RPG.FluentInterface.Types.ExpressionBuilder;
+using System;
+
+namespace OOD.RPG.FluentInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            var builder = new CharacterBuilder();
+            builder
+                .Create("Valient")
+                .As(Types.ClassType.Fighter)
+                .WithAge(22)
+                .HP(20)
+                .Strength(18)
+                .Agi(14)
+                .Int(12);
+            var hero = builder.Value();
+            while (hero.IsAlive)
+            {
+                builder.Create(CommonHelper.GenerateRandomName())
+                    .As((Types.ClassType)ran.Next(4))
+                    .WithAge(ran.Next(12,200))
+                    .HP(ran.Next(5,12))
+                    .Strength(ran.Next(21))
+                    .Agi(ran.Next(21))
+                    .Int(12)
+                    .Gold(ran.Next(50));
+
+                var enemy = builder.Value();
+                CommonHelper.DisplayStartOfBattle(hero, enemy);
+                CommonHelper.Battle(hero, enemy);
+            }
+            Console.ReadKey();
+        }
+    }
+}
