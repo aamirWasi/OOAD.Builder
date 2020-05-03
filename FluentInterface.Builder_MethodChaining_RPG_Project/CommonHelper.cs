759abebcbb7e53aac6cb67b797230c63f47f12b4
aamir <wasi3aamir@gmail.com>
2020-05-03 23:06:48 +0600

    now all codes are worked fine

---
+++ FluentInterface.Builder_MethodChaining_RPG_Project/CommonHelper.cs
@@ -0,0 +1,80 @@
+﻿using FluentInterface.Builder_MethodChaining_RPG_Project_Types;
+using System;
+using System.Threading;
+
+namespace FluentInterface.Builder_MethodChaining_RPG_Project
+{
+    public static class CommonHelper
+    {
+        private static string[] _attackVerbs =
+        {
+            "Slashes","Smashes","Shoots","Stabs","Impales","Destroys","Incinerates","Poisons"
+        };
+        private static string[] _firstNames =
+        {
+            "Destro","Victo","Mozri","Fang","Ovi","Hell","Syth","End"
+        };
+        private static string[] _lastNames =
+        {
+            "math","rin","sith","icous","ravage","wrath","ryn","less"
+        };
+        public static string GenerateRandomName()
+        {
+            string result = "";
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            result = $"{_firstNames[ran.Next(_firstNames.Length)]}{_lastNames[ran.Next(_lastNames.Length)]}";
+            return result;
+        }
+        public static void DisplayStartOfBattle(Character hero,Character enemy)
+        {
+            Console.WriteLine("<=Starting Battle=>");
+            Console.WriteLine(hero.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Vs.");
+            Console.WriteLine();
+            Console.WriteLine(enemy.ToString());
+            Console.WriteLine();
+            Console.WriteLine("An enemy approaches");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static void Battle(Character hero,Character enemy)
+        {
+            while (hero.IsAlive&&enemy.IsAlive)
+            {
+                Console.WriteLine($"({hero.QuickStats()}) / ({enemy.QuickStats()})");
+                Thread.Sleep(1000);
+                Console.WriteLine(Attack(hero,enemy));
+                Thread.Sleep(1000);
+                Console.WriteLine(Attack(enemy,hero));
+                if (hero.IsAlive)
+                {
+                    Console.Write(">");
+                    Console.ReadKey();
+                }
+            }
+            if (hero.IsAlive)
+            {
+                hero.Gold += enemy.Gold;
+                Console.WriteLine($"Our hero survives to fight another battle!.Won {enemy.Gold} gold");
+                Console.WriteLine(">");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+                Console.WriteLine($"Our hero has fallen with {hero.Gold} gold!. The World is covered in darkness once again");
+        }
+
+        private static string Attack(Character attacker, Character defender)
+        {
+            string result = "";
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            int damage = ran.Next(10);
+            defender.HP -= damage;
+            string verb = _attackVerbs[ran.Next(_attackVerbs.Length)];
+            result = $"{attacker.Name} {verb} {defender.Name} for {damage} damage";
+            return result;
+        }
+    }
+}
---
+++ FluentInterface.Builder_MethodChaining_RPG_Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using FluentInterface.Builder_MethodChaining_RPG_Project_Types.ExpressionBuilder;
+using System;
+
+namespace FluentInterface.Builder_MethodChaining_RPG_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random((int)DateTime.Now.Ticks);
+            var builder = new CharacterBuilder();
+            builder
+                .Create("Valient")
+                .As(Builder_MethodChaining_RPG_Project_Types.ClassType.Fighter)
+                .WithAge(22)
+                .SetHP(20)
+                .SetIntelligence(12)
+                .SetStrenght(18)
+                .SetAgility(14)
+                .SetGold(0);
+            var hero = builder.CreateCharacter();
+            while (hero.IsAlive)
+            {
+                builder
+                .Create(CommonHelper.GenerateRandomName())
+                .As((Builder_MethodChaining_RPG_Project_Types.ClassType)ran.Next(4))
+                .WithAge(ran.Next(12,200))
+                .SetHP(ran.Next(5,12))
+                .SetIntelligence(ran.Next(21))
+                .SetStrenght(ran.Next(21))
+                .SetAgility(ran.Next(21))
+                .SetGold(ran.Next(50));
+                var enemy = builder.CreateCharacter();
+                CommonHelper.DisplayStartOfBattle(hero, enemy);
+                CommonHelper.Battle(hero, enemy);
+            }
+            Console.ReadKey();
+        }
+    }
+}
