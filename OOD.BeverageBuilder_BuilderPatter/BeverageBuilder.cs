cd81254d5de2dc39673d4f360c39f25b347d05f9
aamir <wasi3aamir@gmail.com>
2020-05-03 08:13:36 +0600

    adding some message in every method in Beveragebuilder

--- OOD.BeverageBuilder_BuilderPatter/BeverageBuilder.cs
+++ OOD.BeverageBuilder_BuilderPatter/BeverageBuilder.cs
@@ -10,18 +10,22 @@
         }
         public void SetWater(int water)
         {
+            System.Console.WriteLine($"Boiling Water");
             _order.Water = water;
         }
         public void SetMilk(int milk)
         {
+            System.Console.WriteLine($"Adding {milk}ml milk");
             _order.Milk = milk;
         }
         public void SetSugar(int sugar)
         {
+            System.Console.WriteLine($"Adding {sugar}gm sugar");
             _order.Sugar = sugar;
         }
         public void SetPowderQuantity(int quantity)
         {
+            System.Console.WriteLine($"Adding {quantity}gm {_order.BeverageName}powder");
             _order.PowderQuantity = quantity;
         }
         public void SetBeverageType(string type)
