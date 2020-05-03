d5aa4de4a49b0afb33743556bf9efc97e6120f95
aamir <wasi3aamir@gmail.com>
2020-05-03 08:29:37 +0600

    for impleting Fluent interface we need the help of meth chaining.

---
+++ FluentInterface/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FluentInterface
+{
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+    public class EmployeeBuilder
+    {
+        private readonly Employee _order;
+
+        public EmployeeBuilder()
+        {
+            _order = new Employee();
+        }
+        public EmployeeBuilder EmployeeName(string name)
+        {
+            _order.FullName = name;
+            return this;
+        }
+        public EmployeeBuilder Born(string date)
+        {
+            _order.DateOfBirth = Convert.ToDateTime(date);
+            return this;
+        }
+        public EmployeeBuilder StaysAt(string address)
+        {
+            _order.Address = address;
+            return this;
+        }
+        public EmployeeBuilder WorkingOn(string department)
+        {
+            _order.Department = department;
+            return this;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new EmployeeBuilder();
+            builder
+                .EmployeeName("aamir khan")
+                .Born("03/02/1996")
+                .WorkingOn("IT")
+                .StaysAt("Chandraghona");
+
+        }
+    }
+}
