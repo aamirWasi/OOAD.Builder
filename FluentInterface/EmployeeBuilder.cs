70367072ff1708e7c6d79d158a60ce5fefe9dfdd
aamir <wasi3aamir@gmail.com>
2020-05-03 08:34:49 +0600

    classes separate from main

---
+++ FluentInterface/EmployeeBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FluentInterface
+{
+    public class EmployeeBuilder
+    {
+        private readonly Employee _order;
+
+        public EmployeeBuilder()
+        {
+            _order = new Employee();
+        }
+        public EmployeeBuilder EmployeeName(string name)
+        {
+            _order.FullName = name;
+            return this;
+        }
+        public EmployeeBuilder Born(string date)
+        {
+            _order.DateOfBirth = Convert.ToDateTime(date);
+            return this;
+        }
+        public EmployeeBuilder StaysAt(string address)
+        {
+            _order.Address = address;
+            return this;
+        }
+        public EmployeeBuilder WorkingOn(string department)
+        {
+            _order.Department = department;
+            return this;
+        }
+    }
+}
--- FluentInterface/Program.cs
+++ FluentInterface/Program.cs
@@ -1,43 +1,5 @@
-﻿using System;
-
-namespace FluentInterface
+﻿namespace FluentInterface
 {
-    public class Employee
-    {
-        public string FullName { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Address { get; set; }
-        public string Department { get; set; }
-    }
-    public class EmployeeBuilder
-    {
-        private readonly Employee _order;
-
-        public EmployeeBuilder()
-        {
-            _order = new Employee();
-        }
-        public EmployeeBuilder EmployeeName(string name)
-        {
-            _order.FullName = name;
-            return this;
-        }
-        public EmployeeBuilder Born(string date)
-        {
-            _order.DateOfBirth = Convert.ToDateTime(date);
-            return this;
-        }
-        public EmployeeBuilder StaysAt(string address)
-        {
-            _order.Address = address;
-            return this;
-        }
-        public EmployeeBuilder WorkingOn(string department)
-        {
-            _order.Department = department;
-            return this;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
